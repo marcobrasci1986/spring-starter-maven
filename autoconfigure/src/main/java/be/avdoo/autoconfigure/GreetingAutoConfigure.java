f4959c7337426b5b65ca1621dee037f688e8dffe
marcobrasci1986 <marco_brasci@hotmail.com>
2019-04-09 23:12:53 +0200

    working version

--- autoconfigure/src/main/java/be/avdoo/autoconfigure/GreetingAutoConfigure.java
+++ autoconfigure/src/main/java/be/avdoo/autoconfigure/GreetingAutoConfigure.java
@@ -1,21 +1,29 @@
 package be.avdoo.autoconfigure;
 
 import be.avidoo.kring.commons.GreetingService;
+import be.avidoo.kring.commons.config.ObjectMapperConfiguration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
 @ConditionalOnClass(GreetingService.class)
+@Import(ObjectMapperConfiguration.class)
 public class GreetingAutoConfigure {
 
+    @Autowired
+    @Qualifier("kringObjectMapper")
+    private ObjectMapper objectMapper;
 
     @Bean
     @ConditionalOnProperty(prefix = "kring.commons", value = "true")
     public GreetingService greetingService() {
-
-        return new GreetingService();
+        return new GreetingService(objectMapper);
     }
 }
 
--- kring-commons/src/main/java/be/avidoo/kring/commons/config/ObjectMapperConfiguration.java
+++ kring-commons/src/main/java/be/avidoo/kring/commons/config/ObjectMapperConfiguration.java
@@ -1,38 +1,35 @@
-//package be.avidoo.kring.commons.config;
-//
-//import com.fasterxml.jackson.annotation.JsonInclude;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-//import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-//import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.time.LocalDateTime;
-//import java.time.format.DateTimeFormatter;
-//
-//@Configuration
-//public class ObjectMapperConfiguration {
-//
-//    @Bean
-//    public ObjectMapper objectMapper() {
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        objectMapper.registerModule(createJavaTimeModule());
-//
-//
-//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-//
-//        return objectMapper;
-//    }
-//
-//    private JavaTimeModule createJavaTimeModule() {
-//
-//
-//
-//        JavaTimeModule javaTimeModule = new JavaTimeModule();
-//        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-//        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-//        return javaTimeModule;
-//    }
-//}
+package be.avidoo.kring.commons.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class ObjectMapperConfiguration {
+
+    @Bean(name = "kringObjectMapper")
+    public ObjectMapper kringObjectMapper() {
+        System.out.println("Custom ObjectMapper has been made !!");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(createJavaTimeModule());
+
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        return objectMapper;
+    }
+
+    private JavaTimeModule createJavaTimeModule() {
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        return javaTimeModule;
+    }
+}
